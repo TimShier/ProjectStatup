1c3a6c7cf61fc0fc38fee0828741b43f0144f1f6
theDanielK <dankruss@gmail.com>
2013-07-28 19:05:13 +0200

    bootstrap my bitch up

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,18 +1,16 @@
 import startupfunnel.app.User
 import startupfunnel.app.enums.RoleType
+import startupfunnel.app.Project
+import startupfunnel.app.Contact
+import startupfunnel.app.enums.RolePosition
 
 class BootStrap {
 
     def init = { servletContext ->
 
-        //Users
-        /*String  emailAddress,
-                firstName,
-                lastName,
-                password // encrypted.
-
-        RoleTypeEnum role    */
-
+        /*
+         *    --- USERS ---
+        */
         User admin = new User(
                 emailAddress: "admin@projectstartup.com",
                 firstName: "admin",
@@ -26,7 +24,15 @@ class BootStrap {
                 firstName: "student",
                 lastName: "student",
                 password: "password",
-                role: RoleType.ADMIN
+                role: RoleType.STUDENT
+        ).save()
+
+        User student2 = new User(
+                emailAddress: "student2@projectstartup.com",
+                firstName: "student",
+                lastName: "student",
+                password: "password",
+                role: RoleType.STUDENT
         ).save()
 
         User mentor = new User(
@@ -34,7 +40,7 @@ class BootStrap {
                 firstName: "mentor",
                 lastName: "mentor",
                 password: "password",
-                role: RoleType.ADMIN
+                role: RoleType.MENTOR
         ).save()
 
         User adjudicator = new User(
@@ -42,10 +48,8 @@ class BootStrap {
                 firstName: "adjudicator",
                 lastName: "adjudicator",
                 password: "password",
-                role: RoleType.ADMIN
+                role: RoleType.ADJUDICATOR
         ).save()
-
-
 
         User tim = new User(
                  emailAddress: "tim.shier@instantafrica.com",
@@ -55,6 +59,33 @@ class BootStrap {
                  role: RoleType.ADMIN
          ).save()
 
+
+        /*
+         *    --- PROJECTS ---
+        */
+        Project project1 = new Project(
+                name: "Project One",
+                startDate: new Date(),
+                presentationDate: new Date()
+        ).save()
+
+
+        /*
+         *    --- CONTACTS ---
+        */
+        Contact lead = new Contact(
+                user: student,
+                role: RoleType.STUDENT,
+                position: RolePosition.LEAD,
+                project: project1
+        ).save()
+
+        Contact member = new Contact(
+                user: student2,
+                role: RoleType.STUDENT,
+                position: RolePosition.MEMBER,
+                project: project1
+        ).save()
 
     }
     def destroy = {
