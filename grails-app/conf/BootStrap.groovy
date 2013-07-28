2eb905fc9326a9ae24450ead2eeb2ae153ad1bc8
TimShier <tim.shier@gmail.com>
2013-07-28 18:43:24 +0200

    Generate-all on Stage + Bootstrap throw aways

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,28 @@
+import startupfunnel.app.User
+
 class BootStrap {
 
     def init = { servletContext ->
+        setupForLocal()
     }
     def destroy = {
+    }
+
+    def setupForLocal(){
+        // do basic local bootstrap. All the shit and the kitchen sink.
+
+        //User tim = new User(emailAddress: "tim.shier@instantafrica.com", "Timbo", "Shier","12345", role:)
+
+        /*
+        * String emailAddress
+    String firstName
+    String lastName
+    String password // encrypted.
+
+
+
+    RoleTypeEnum role
+        *
+        * */
     }
 }
