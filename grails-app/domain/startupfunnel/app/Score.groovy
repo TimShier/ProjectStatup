41be43dc8104231a5f66f932156ebf31e1e0fe32
TimShier <tim.shier@gmail.com>
2013-07-28 21:12:07 +0200

    Score updated with approved (Bool)

--- grails-app/domain/startupfunnel/app/Score.groovy
+++ grails-app/domain/startupfunnel/app/Score.groovy
@@ -5,10 +5,12 @@ class Score {
     Integer value
     String feedback
     Date dateCreated
+    Boolean approved = false
 
     static belongsTo = [answer:Answer]
 
     static constraints = {
+        approved(nullable: true)
 
     }
 }
